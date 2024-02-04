85f540199f29066a0216603cb6244f0d96b05041
fs-projects <fs.projectsuk@gmail.com>
2024-02-04 20:38:03 +0530

    Add: skeleton server actions for app

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,11 +1,5 @@
-"use server";
-
-import * as auth from "@/auth";
-
-export async function signIn() {
-  return auth.signIn("github");
-}
-
-export async function signOut() {
-  return auth.signOut();
-}
+export { signIn } from "./sign-in";
+export { signOut } from "./sign-out";
+export { createTopic } from "./createTopic";
+export { createPost } from "./createPost";
+export { createComment } from "./createComment";
